47758903cd730bc480995f32b59c768788cf85f0
mk <107159868+cv-any@users.noreply.github.com>
2023-04-26 13:48:08 +0800

    Create serve.js

---
+++ serve.js
@@ -0,0 +1,44 @@
+import { ChatGPTAPI } from "chatgpt";
+import express from 'express';
+import bodyParser from 'body-parser';
+import fetch from 'node-fetch';
+
+const apiKey = "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";
+const app = express()
+const port = 3000;
+app.use(bodyParser.json());// 添加json解析
+app.use(bodyParser.urlencoded({extended: false}));
+//设置跨域访问
+// app.all('*', function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Content-Type", "application/json;charset=utf-8");
+//   next();
+// });
+
+
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By",' 3.2.1');
+  if(req.method=="OPTIONS") res.send(200);/*让options请求快速返回*/
+  else next();
+});
+const chatGPTApi = new ChatGPTAPI({
+  apiKey,
+  fetch,
+});
+
+app.get('/sendMsg', async (req, res) => {
+  const { query, body } = req;
+  console.log({ query, body, }, 'req <<<<<<<')
+  // let result = await chatGPTApi.sendMessage(req.body.msg)
+  let result = await chatGPTApi.sendMessage(req.query.q)
+  console.log('res==>', result)
+  res.send(`${result.text}`)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
