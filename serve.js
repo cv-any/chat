2161384556d89590a062ec6caadf97a33342c41f
qiao <qiaomin_email@163.com>
2023-09-05 20:42:53 +0800

    dev

--- serve.js
+++ serve.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 //   next();
 // });
 
-
+// test
 app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", req.headers.origin);
   res.header("Access-Control-Allow-Credentials", "true");
